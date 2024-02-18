dbaa9119cc498c829f928c24d57649f6c9d7a444
Rodrigo Madeiro <rodrigomadeiro@Rodrigos-MacBook-Air.local>
2024-02-18 12:55:52 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"O lado mais belo de Jampa",
+    "description": "Descubra as praias paradisiacas e mais bonitas do Brasil, com uma calmaria e aconchego Joao Pessoa se torna um dos destinos mais cobicados pelos turistas.",
+    "photoCover": "https://www.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Praia Bela",
+    "description": "Descubra as belezas de Praia Bela",
+    "photoCover": "https://www.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Praia do Amor",
+    "description": "Descubra as belezas de Praia Bela",
+    "photoCover": "https://www.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
+  }
+]
